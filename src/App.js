2f033a0ca9b96c74a6418e3b61f4d9755aa8a201
dldmswl090 <79970323+dldmswl090@users.noreply.github.com>
2022-06-02 15:59:15 +0900

    add homepage.js modify

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Homepage from './Pages/Homepage';
 import Questionpage from './Pages/Questionpage';
 import Resultpage from './Pages/Resultpage';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Homepage/>
-      <Questionpage/>
-      <Resultpage/>
-    </div>
+    
+    <Routes>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="/Questionpage" element={<Questionpage/>}/>
+      <Route path="/Resultpage" element={<Resultpage/>}/>
+    </Routes>
+   
   );
 }
 
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import PickImg from '../assets/dogs/logoimage.jpg';
+import Button from 'react-bootstrap/Button';
+import  {useNavigate} from 'react-router-dom';
+
+
 
 const Homepage = () => {
+
+  const navigate = useNavigate();
+const handleClicktest = () => { 
+    navigate('/Questionpage');
+ }
+
   return (
     <Wrap>
         <Header>나와 가장 잘 맞는 댕댕이를 찾아라멍!</Header>
-        <LogoImg><img src={PickImg} alt="대표강아지 사진" className="round-circle" width={300} height={300}/></LogoImg> 
+        <LogoImg>
+          <img alt="대표강아지 사진" src={PickImg} className="rounded-circle"/>
+        </LogoImg>
+        <Desc>나와 가장 잘 맞는 댕댕이를 지금부터 찾아볼까요?</Desc> 
+        <Button style={{backgroundColor:'lightgray' , color:'black', fontSize: '18px', border:'none', outLine:'none', padding: '15px'}} onClick={handleClicktest}>테스트 시작하기</Button>
     </Wrap>
   )
 }
@@ -16,11 +30,21 @@ export default Homepage;
 const Wrap = styled.div`
 height: 100vh;
 width:100%;
+display: flex;
+justify-content:center;
+align-items: center;
+flex-direction: column;
+
 `
 const Header = styled.div`
-background-color: yellow; 
+font-size: 27px;
+font-weight: bold;
 `
 const LogoImg = styled.div`
-margin-top: 20px;
+margin-top: 40px;
+margin-bottom: 40px;
 `
-
+const Desc = styled.div`
+font-size: 20px;
+margin-bottom: 40px;
+`
